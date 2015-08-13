d93dcc60a19b0e5f0ee872c1d2d58b1da7f0656c
Denis Torgunov <dtorgunov@leafonthewind.net>
2015-08-13 17:28:57 +0300

    Abstract the error reporting
    This removes some code duplication and resolves #3.

--- server.go
+++ server.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
 )
 
 type Request struct {
 	Filename string
-	Mime string
-	Length int
+	Mime     string
+	Length   int
 }
 
 // These configuration variables will be read from command line flags later
 var (
-	address = "localhost:3030"
+	address   = "localhost:3030"
 	directory = "data"
 )
 
+func writeError(w http.ResponseWriter, code int, message string) {
+	w.WriteHeader(code)
+	fmt.Fprintf(w, "%s\r\n", message)
+}
+
 func transferRequestHandler(w http.ResponseWriter, req *http.Request) {
 	var r Request
-	
+
 	// Need to handle OPTIONS properly
-	if (req.Method != "POST") {
+	if req.Method != "POST" {
 		w.Header().Add("Allow", "POST")
 		w.WriteHeader(405)
 		return
@@ -31,20 +36,17 @@ func transferRequestHandler(w http.ResponseWriter, req *http.Request) {
 	// Need to check content-type
 	err := json.NewDecoder(req.Body).Decode(&r)
 	if err != nil {
-		w.WriteHeader(400)
-		fmt.Fprintf(w, "The JSON data you have sent is invalid.\r\n")
+		writeError(w, 400, "The JSON data you have sent is invalid.")
 		return
 	}
 
 	if r.Length <= 0 {
-		w.WriteHeader(400)
-		fmt.Fprintf(w, "Valid length needs to be specified\r\n")
+		writeError(w, 400, "Valid length needs to be specified.")
 		return
 	}
 
 	if r.Filename == "" && r.Mime == "" {
-		w.WriteHeader(400)
-		fmt.Fprintf(w, "Either a filename or MIME-type needs to be specified\r\n")
+		writeError(w, 400, "Either a filename or MIME-type needs to be specified")
 		return
 	}
 
@@ -56,9 +58,9 @@ func main() {
 	fmt.Printf("%s => %s\n", address, directory)
 	http.HandleFunc("/", transferRequestHandler)
 
-	err := http.ListenAndServe(address,nil)
+	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		fmt.Printf("Error occured")
 	}
-	
+
 }
