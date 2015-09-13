4fea4f412f6a45fcbd8ef28e6d6195fa052d8b99
Denis Torgunov <dtorgunov@leafonthewind.net>
2015-09-14 00:01:20 +0300

    Add flags to the executable
    Added -address and -datadir flags to allow the user to specify what
    address/port to listen on, as well as to choose the directory into which
    the uploaded files will be saved.

--- himawarid/main.go
+++ himawarid/main.go
@@ -1,7 +1,33 @@
+// himawari. A simple file upload handling server.
+// Copyright (C) 2015 Denis Torgunov
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// This package is used to start a basic himawari server
 package main
 
-import "github.com/dtorgunov/himawari"
+import (
+	"flag"
+	"github.com/dtorgunov/himawari"
+)
+
+var (
+	address = flag.String("address", ":3030", "the address to listen on")
+	datadir = flag.String("datadir", "data", "the directory to save the uploaded files to")
+)
 
 func main() {
-	himawari.StartServer("localhost:3030", "data")
+	flag.Parse()
+	himawari.StartServer(*address, *datadir)
 }
